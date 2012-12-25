cb35ac7331ad712f79a27a1f586e6db96d66196c
Evan Kroske <evan@evankroske.com>
2012-12-25 16:53:21 -0600

    Made schema more explicit.

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -5,13 +5,13 @@
 create table usf_restaurants (
 id serial primary key,
 name varchar(100),
-sunday_hours bigint,
-monday_hours bigint,
-tuesday_hours bigint,
-wednesday_hours bigint,
-thursday_hours bigint,
-friday_hours bigint,
-saturday_hours bigint);
+sunday_hours bit(48),
+monday_hours bit(48),
+tuesday_hours bit(48),
+wednesday_hours bit(48),
+thursday_hours bit(48),
+friday_hours bit(48),
+saturday_hours bit(48));
 
 # --- !Downs
 
