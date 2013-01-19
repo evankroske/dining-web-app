f4f5a0af0dba9569b0a1303b2f2e589afae2ad7f
Evan Kroske <evan@evankroske.com>
2013-01-19 13:05:25 -0500

    Began implementing check for which restaurants are open using moment.js time library.

--- public/javascripts/restaurant-view-model.js
+++ public/javascripts/restaurant-view-model.js
@@ -1,8 +1,30 @@
+function ObservableBitSet(data) {
+	var self = this;
+	self.contains = function(i) {
+		return (i < data.length * 48 &&
+			(data[Math.floor(i / 48)] & (1 << (i % 48))));
+	}
+}
+
 function RestaurantViewModel(data) {
 	var self = this;
 	self.id = data.id;
 	self.name = ko.observable(data.name);
-	self.hours = ko.observableArray(data.hours);
+	self.hours = data.hours.map(function (e) {
+		ko.observable(e);
+	});
+	self.hoursBitSet = new ObservableBitSet(self.hours);
+	self.open = ko.computed(function() {
+		var now = moment();
+		var nowUtcNoDst = now.clone().utc();
+		var halfHrIndex = nowUtcNoDst.day() * 48 +
+			nowUtcNoDst.hours() * 2 +
+			Math.floor(nowUtcNoDst.minutes() / 30);
+		return self.hoursBitSet.contains(halfHrIndex);
+	});
+	self.toString = function() {
+		return "[RestaurnantViewModel]";
+	}
 }
 
 function RestaurantsViewModel() {
@@ -10,12 +32,23 @@ function RestaurantsViewModel() {
 
 	self.restaurants = ko.observableArray();
 	self.chosenRestaurant = ko.observable();
+	self.open = ko.computed(function() {
+		return ko.utils.unwrapObservable(self.restaurants).filter(function(r) {
+			return r.open();
+		});
+	});
+	self.closed = ko.computed(function() {
+		return ko.utils.unwrapObservable(self.restaurants).filter(function(r) {
+			return !r.open();
+		});
+	});
 
 	Sammy(function() {
 		this.get("#!restaurants", function() {
 			self.chosenRestaurant(null);
 			$.getJSON("/api/usf/restaurants", null, function(data) {
-				self.restaurants.unshift.apply(self.restaurants, data);
+				self.restaurants.unshift.apply(self.restaurants,
+					data.map(function(r) { return new RestaurantViewModel(r); }));
 			});
 		});
 		this.get("", function() { this.app.runRoute("get", "#!restaurants"); });
