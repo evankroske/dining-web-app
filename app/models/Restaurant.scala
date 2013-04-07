d11f1aacbb6aae0e270a6c5f0b8d9cc3b190d7c6
Evan Kroske <evan@evankroske.com>
2013-04-07 16:42:50 -0400

    Rewrote BitSet serialization using Play 2.1 JSON combinators.

--- app/models/Restaurant.scala
+++ app/models/Restaurant.scala
@@ -4,51 +4,43 @@ import anorm._
 import anorm.SqlParser._
 import play.api.db._
 import play.api.Play.current
+import play.api.libs.json._
+import play.api.libs.functional.syntax._
 
-import java.util.{TimeZone, Date, Calendar}
-import java.text.{DateFormat, SimpleDateFormat}
-
-import scala.collection.BitSet
-import scala.collection.mutable.MutableList
-
-import scala.collection.immutable.BitSet.{fromArray => bitSetFromArray}
-import scala.math._
-import java.lang.Integer.parseInt
+import scala.collection.immutable.BitSet
 
 case class Restaurant(val id: Int, val name: String, val hours: BitSet)
 
 object Restaurant {
-	implicit def bitSet2JsNumberSeq(b: BitSet): Seq[JsNumber] = {
-		val numInts = (ceil((b max) / 32.0)).toInt
-		(0 until numInts) map { i =>
-			JsNumber(parseInt(
-					(30 to 0 by -1) map { j =>
-						if (b contains (i * 32 + j)) '1'
-						else '0'
-					} mkString, 2) | (if (b contains (i * 32 + 31)) 1 << 31 else 0))
-		}
+	val bitSetReads: Reads[BitSet] =
+		__.read[Seq[Int]].map[Array[Long]] { l =>
+			l.grouped(2).map {
+				case List(lsb, msb) => msb.toLong << 32 | lsb.toLong & 0xffffffffL
+				case List(msb) => msb.toLong << 32
+			}.toArray
+		}.map(BitSet.fromBitMask _)
+	
+	val bitSetWrites: Writes[BitSet] = new Writes[BitSet] {
+		def writes(b: BitSet): JsValue = Json.toJson(b.toBitMask.flatMap { mask =>
+			List(mask.toInt, mask >> 32 toInt)
+		})
 	}
 
-	implicit val bitSetFormat: Format[BitSet] = (
-		__.read[List[Int]].grouped(2).map {
-			case List(lsb, msb) => Long(msb) << 32 | Long(lsb) && 0xffffffffL
-			case List(msb) => Long(msb) << 32
-	)(BitSet.fromBitMask, BitSet.toBitMask)
+	implicit val bitSetFormat = Format(bitSetReads, bitSetWrites)
+
 	implicit val restaurantFormat: Format[Restaurant] = (
-		(__ \ "id").read[Int] ~
-		(__ \ "name").read[String] ~ 
-		(__ \ "hours").read[List[Long]].map { l =>
-			l.zipWithIndex
+		(__ \ "id").format[Int] ~
+		(__ \ "name").format[String] ~ 
+		(__ \ "hours").format[BitSet]
 	)(Restaurant.apply, unlift(Restaurant.unapply))
 
 	val restaurant = {
 		int("id") ~ str("name") ~ str("hours") map {
-				case id ~ name ~ hours => {
+				case id ~ name ~ hours =>
 					val weekBitSet = BitSet((hours zipWithIndex) collect {
 						case ('1', i) => i
 					}: _*)
 					Restaurant(id, name, weekBitSet)
-				}
 			}
 	}
 
